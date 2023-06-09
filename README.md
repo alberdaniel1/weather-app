3096cce7e73426c744224edf28a12fb95d5402e2
Daniel Avelar <danielavelar@Daniels-MacBook-Pro.local>
2023-06-08 20:18:34 -0400

    added more functions

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # weather-app
+
+
+## Features
+
+- The application is loaded and a welcome page is displayed with a greeting and on the sidebar the user can find the search input;
+- Once the desired location is input and user click search a page displays the current weather forecast for that location and for the following 5 days;
+- The infromation provedide includes temperature, humidity, speed limit, feels like and UV index;
+- For the current weather and 5 days forecast a icon represents the weather conditions;
+- If user search for a city and it cant be found a error message is displayed instructing user to enter city name again;
+- The results for the searched cities are stoared at users local storage and a clear history button clear the local storage if user desire.
+
+
+## Development
+
+## Authors
+
+Alberto Avelar
--- script.js
+++ script.js
@@ -1,1 +1,86 @@
 
+//Declare a variables
+var city="";
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty= $("#humidity");
+var currentWSpeed=$("#wind-speed");
+var currentUvindex= $("#uv-index");
+var sCity=[];
+
+
+
+function find(c){
+    for (var i=0; i<sCity.length; i++){
+        if(c.toUpperCase()===sCity[i]){
+            return -1;
+        }
+    }
+    return 1;
+}
+
+var APIKey="a0aca8a89948154a4182dcecc780b513";
+
+
+function displayWeather(event){
+    event.preventDefault();
+    if(searchCity.val().trim()!==""){
+        city=searchCity.val().trim();
+        currentWeather(city);
+    }
+}
+
+
+function currentWeather(city){
+   
+    var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+    $.ajax({
+        url:queryURL,
+        method:"GET",
+    }).then(function(response){
+
+        console.log(response);
+     
+        var weathericon= response.weather[0].icon;
+        var iconurl="https://openweathermap.org/img/wn/"+weathericon +"@2x.png";
+
+        var date=new Date(response.dt*1000).toLocaleDateString();
+    
+        $(currentCity).html(response.name +"("+date+")" + "<img src="+iconurl+">");
+     
+
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        $(currentTemperature).html((tempF).toFixed(2)+"&#8457");
+    
+        $(currentHumidty).html(response.main.humidity+"%");
+       
+        var ws=response.wind.speed;
+        var windsmph=(ws*2.237).toFixed(1);
+        $(currentWSpeed).html(windsmph+"MPH");
+  
+        UVIndex(response.coord.lon,response.coord.lat);
+        forecast(response.id);
+        if(response.cod==200){
+            sCity=JSON.parse(localStorage.getItem("cityname"));
+            console.log(sCity);
+            if (sCity==null){
+                sCity=[];
+                sCity.push(city.toUpperCase()
+                );
+                localStorage.setItem("cityname",JSON.stringify(sCity));
+                addToList(city);
+            }
+            else {
+                if(find(city)>0){
+                    sCity.push(city.toUpperCase());
+                    localStorage.setItem("cityname",JSON.stringify(sCity));
+                    addToList(city);
+                }
+            }
+        }
+
+    });
+}
