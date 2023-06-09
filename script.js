5102f99eb4ba2544d92861662bb0ed4d10210158
Daniel Avelar <danielavelar@Daniels-MacBook-Pro.local>
2023-06-08 23:11:08 -0400

    added fetchcalls

--- script.js
+++ script.js
@@ -84,3 +84,86 @@ function currentWeather(city){
 
     });
 }
+function UVIndex(ln,lt){
+
+  var uvqURL="https://api.openweathermap.org/data/2.5/uvi?appid="+ APIKey+"&lat="+lt+"&lon="+ln;
+  $.ajax({
+          url:uvqURL,
+          method:"GET"
+          }).then(function(response){
+              $(currentUvindex).html(response.value);
+          });
+}
+  
+//displaying the 5 days forecast for the current city.
+function forecast(cityid){
+  var dayover= false;
+  var queryforcastURL="https://api.openweathermap.org/data/2.5/forecast?id="+cityid+"&appid="+APIKey;
+  $.ajax({
+      url:queryforcastURL,
+      method:"GET"
+  }).then(function(response){
+      
+      for (i=0;i<5;i++){
+          var date= new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
+          var iconcode= response.list[((i+1)*8)-1].weather[0].icon;
+          var iconurl="https://openweathermap.org/img/wn/"+iconcode+".png";
+          var tempK= response.list[((i+1)*8)-1].main.temp;
+          var tempF=(((tempK-273.5)*1.80)+32).toFixed(2);
+          var humidity= response.list[((i+1)*8)-1].main.humidity;
+      
+          $("#fDate"+i).html(date);
+          $("#fImg"+i).html("<img src="+iconurl+">");
+          $("#fTemp"+i).html(tempF+"&#8457");
+          $("#fHumidity"+i).html(humidity+"%");
+      }
+      
+  });
+}
+
+
+
+function addToList(c){
+  var listEl= $("<li>"+c.toUpperCase()+"</li>");
+  $(listEl).attr("class","list-group-item");
+  $(listEl).attr("data-value",c.toUpperCase());
+  $(".list-group").append(listEl);
+}
+
+function invokePastSearch(event){
+  var liEl=event.target;
+  if (event.target.matches("li")){
+      city=liEl.textContent.trim();
+      currentWeather(city);
+  }
+
+}
+
+
+function loadlastCity(){
+  $("ul").empty();
+  var sCity = JSON.parse(localStorage.getItem("cityname"));
+  if(sCity!==null){
+      sCity=JSON.parse(localStorage.getItem("cityname"));
+      for(i=0; i<sCity.length;i++){
+          addToList(sCity[i]);
+      }
+      city=sCity[i-1];
+      currentWeather(city);
+  }
+
+}
+
+
+function clearHistory(event){
+  event.preventDefault();
+  sCity=[];
+  localStorage.removeItem("cityname");
+  document.location.reload();
+
+
+
+$("#search-button").on("click",displayWeather);
+$(document).on("click",invokePastSearch);
+$(window).on("load",loadlastCity);
+$("#clear-history").on("click",clearHistory);
